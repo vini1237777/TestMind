9a66427582105e268acbf2bab756ff919a4ee2b2
Vinisha Yadav <vinishayadav@Vinishas-MacBook-Air.local>
2025-11-17 15:21:51 +0530

    modified: the margins of input form 2

--- app/page.tsx
+++ app/page.tsx
@@ -103,208 +103,215 @@ export default function HomePage() {
       <nav className="w-full bg-amber-200 h-12 flex items-center px-4">
         <h1 className="font-semibold">TestMind</h1>
       </nav>
-      <section className="w-full flex flex-col gap-4 max-w-5xl mt-10 mb-10 ml-6 mr-6">
-        <label className="text-sm font-medium">Feature Description</label>
-        <input
-          type="text"
-          placeholder="Feature Name"
-          value={featureName}
-          onChange={(e) => setFeatureName(e.target.value)}
-          className="p-2 border border-gray-300 rounded"
-        />
-        <label className="text-sm font-medium">Description Name</label>
-        <textarea
-          placeholder="Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          className="p-2 border border-gray-300 rounded"
-        />
-        <button
-          className="bg-amber-400 p-2 rounded hover:bg-amber-500 cursor-pointer w-full sm:w-auto"
-          onClick={() => {
-            handleGenerate(featureName, description);
-          }}
-          disabled={!featureName.trim() || !description.trim() || isGenerating}
-        >
-          {isGenerating ? "Generating..." : "Generate Test Cases"}{" "}
-        </button>
-        {error && <p className="text-sm text-red-600">{error}</p>}
-        <section className="w-full max-w-2xl mt-8 mb-8">
-          {testCases.length === 0 ? (
-            <p className="text-gray-500">
-              No test cases yet. Click <strong>Generate Test Cases</strong>.
-            </p>
-          ) : (
-            <ul className="flex flex-col gap-4">
-              <h2 className="text-lg font-semibold">
-                Latest Generated Test Cases
-              </h2>
-
-              {testCases?.map((tc) => (
-                <li
-                  key={tc?.id}
-                  className="border border-gray-200 rounded p-3 shadow-sm"
-                >
-                  <div className="flex justify-between text-sm mb-1">
-                    <span className="font-mono text-xs">{tc.id}</span>
-                    <span className="uppercase text-xs text-gray-500">
-                      {tc.type}
-                    </span>
-                  </div>
-                  <h3 className="font-semibold mb-1">{tc.title}</h3>
-                  <div className="mb-1">
-                    <p className="font-semibold text-sm">Steps:</p>
-                    <ol className="list-decimal list-inside text-sm text-gray-700">
-                      {tc.steps.map((step, i) => (
-                        <li key={i}>{step}</li>
-                      ))}
-                    </ol>
-                  </div>
-                  <p className="text-sm">
-                    <span className="font-semibold">Expected:</span>{" "}
-                    {tc.expected}
-                  </p>
-                </li>
-              ))}
-            </ul>
-          )}
-        </section>
-        <section className="w-full max-w-4xl mt-8 flex gap-6">
-          <aside className="w-1/3 border-r pr-4">
-            <h2 className="font-semibold mb-2 text-sm">Test Suites</h2>
-            {testSuites.length === 0 ? (
-              <p className="text-gray-500 text-sm">
-                No suites yet. Generate your first one.
+      <div className="max-w-5xl mx-auto px-4 py-8 w-screen">
+        <section className="flex flex-col gap-4 max-w-5xl mt-10 mb-10 ml-6 mr-6">
+          <label className="text-sm font-medium">Feature Description</label>
+          <input
+            type="text"
+            placeholder="Feature Name"
+            value={featureName}
+            onChange={(e) => setFeatureName(e.target.value)}
+            className="p-2 border border-gray-300 rounded"
+          />
+          <label className="text-sm font-medium">Description Name</label>
+          <textarea
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            className="p-2 border border-gray-300 rounded"
+          />
+          <button
+            className="bg-amber-400 p-2 rounded hover:bg-amber-500 cursor-pointer w-full sm:w-auto"
+            onClick={() => {
+              handleGenerate(featureName, description);
+            }}
+            disabled={
+              !featureName.trim() || !description.trim() || isGenerating
+            }
+          >
+            {isGenerating ? "Generating..." : "Generate Test Cases"}{" "}
+          </button>
+          {error && <p className="text-sm text-red-600">{error}</p>}
+          <section className="w-full max-w-2xl mt-8 mb-8">
+            {testCases.length === 0 ? (
+              <p className="text-gray-500">
+                No test cases yet. Click <strong>Generate Test Cases</strong>.
               </p>
             ) : (
-              <ul className="flex flex-col gap-2">
-                <h2 className="text-lg font-semibold mb-2">
-                  Saved Test Suites
+              <ul className="flex flex-col gap-4">
+                <h2 className="text-lg font-semibold">
+                  Latest Generated Test Cases
                 </h2>
-                {testSuites.map((suite) => (
+
+                {testCases?.map((tc) => (
                   <li
-                    key={suite.id}
-                    onClick={() => {
-                      setSelectedSuiteId(suite.id);
-                      setFilterType("all");
-                    }}
-                    className={
-                      suite.id === selectedSuiteId
-                        ? "p-2 rounded cursor-pointer bg-amber-50 border border-amber-200"
-                        : "p-2 rounded cursor-pointer hover:bg-gray-50 border"
-                    }
+                    key={tc?.id}
+                    className="border border-gray-200 rounded p-3 shadow-sm"
                   >
-                    {suite.name} - {new Date(suite.createdAt).toLocaleString()}
+                    <div className="flex justify-between text-sm mb-1">
+                      <span className="font-mono text-xs">{tc.id}</span>
+                      <span className="uppercase text-xs text-gray-500">
+                        {tc.type}
+                      </span>
+                    </div>
+                    <h3 className="font-semibold mb-1">{tc.title}</h3>
+                    <div className="mb-1">
+                      <p className="font-semibold text-sm">Steps:</p>
+                      <ol className="list-decimal list-inside text-sm text-gray-700">
+                        {tc.steps.map((step, i) => (
+                          <li key={i}>{step}</li>
+                        ))}
+                      </ol>
+                    </div>
+                    <p className="text-sm">
+                      <span className="font-semibold">Expected:</span>{" "}
+                      {tc.expected}
+                    </p>
                   </li>
                 ))}
               </ul>
             )}
-          </aside>
+          </section>
+          <section className="w-full max-w-4xl mt-8 flex gap-6">
+            <aside className="w-1/3 border-r pr-4">
+              <h2 className="font-semibold mb-2 text-sm">Test Suites</h2>
+              {testSuites.length === 0 ? (
+                <p className="text-gray-500 text-sm">
+                  No suites yet. Generate your first one.
+                </p>
+              ) : (
+                <ul className="flex flex-col gap-2">
+                  <h2 className="text-lg font-semibold mb-2">
+                    Saved Test Suites
+                  </h2>
+                  {testSuites.map((suite) => (
+                    <li
+                      key={suite.id}
+                      onClick={() => {
+                        setSelectedSuiteId(suite.id);
+                        setFilterType("all");
+                      }}
+                      className={
+                        suite.id === selectedSuiteId
+                          ? "p-2 rounded cursor-pointer bg-amber-50 border border-amber-200"
+                          : "p-2 rounded cursor-pointer hover:bg-gray-50 border"
+                      }
+                    >
+                      {suite.name} -{" "}
+                      {new Date(suite.createdAt).toLocaleString()}
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </aside>
 
-          <section className="w-2/3 pl-4">
-            {!selectedSuiteId ? (
-              <p className="text-gray-500 text-sm">
-                Select a suite from the left to view test cases.
-              </p>
-            ) : (
-              selectedSuite && (
-                <>
-                  <div className="flex items-center mb-2">
-                    <div>
-                      <h2 className="font-semibold">{selectedSuite.name}</h2>
-                      <p className="text-xs text-gray-500">
-                        {new Date(selectedSuite.createdAt).toLocaleString()}
-                      </p>
-                    </div>
-                    <div className="ml-32 mb-2">
-                      <button
-                        onClick={() => handleDownloadSuite(selectedSuite)}
-                        className="text-xs px-3 py-1 rounded border border-gray-300 hover:bg-gray-50"
-                      >
-                        Download JSON
-                      </button>
-                    </div>
-                  </div>
-
-                  <ul className="flex flex-col gap-4">
-                    <div className="mb-3 flex flex-wrap gap-2">
-                      {tabs.map((tab) => (
+            <section className="w-2/3 pl-4">
+              {!selectedSuiteId ? (
+                <p className="text-gray-500 text-sm">
+                  Select a suite from the left to view test cases.
+                </p>
+              ) : (
+                selectedSuite && (
+                  <>
+                    <div className="flex items-center mb-2">
+                      <div>
+                        <h2 className="font-semibold">{selectedSuite.name}</h2>
+                        <p className="text-xs text-gray-500">
+                          {new Date(selectedSuite.createdAt).toLocaleString()}
+                        </p>
+                      </div>
+                      <div className="ml-32 mb-2">
                         <button
-                          key={tab.value}
-                          onClick={() =>
-                            setFilterType(
-                              tab.value as unknown as
-                                | "all"
-                                | "happy"
-                                | "negative"
-                                | "edge"
-                            )
-                          }
-                          className={
-                            filterType === tab.value
-                              ? "px-3 py-1 rounded-full text-xs font-medium bg-amber-500 text-white"
-                              : "px-3 py-1 rounded-full text-xs font-medium bg-gray-100 text-gray-700 hover:bg-gray-200"
-                          }
+                          onClick={() => handleDownloadSuite(selectedSuite)}
+                          className="text-xs px-3 py-1 rounded border border-gray-300 hover:bg-gray-50"
                         >
-                          {tab.label}
+                          Download JSON
                         </button>
-                      ))}
+                      </div>
                     </div>
-                    <ul className="flex flex-col gap-4">
-                      {filteredCases?.map((tc) => {
-                        const isExpanded = expandedCaseId === tc.id;
 
-                        return (
-                          <li
-                            key={tc?.id}
-                            className="border border-gray-200 rounded p-3 shadow-sm mb-8"
+                    <ul className="flex flex-col gap-4">
+                      <div className="mb-3 flex flex-wrap gap-2">
+                        {tabs.map((tab) => (
+                          <button
+                            key={tab.value}
+                            onClick={() =>
+                              setFilterType(
+                                tab.value as unknown as
+                                  | "all"
+                                  | "happy"
+                                  | "negative"
+                                  | "edge"
+                              )
+                            }
+                            className={
+                              filterType === tab.value
+                                ? "px-3 py-1 rounded-full text-xs font-medium bg-amber-500 text-white"
+                                : "px-3 py-1 rounded-full text-xs font-medium bg-gray-100 text-gray-700 hover:bg-gray-200"
+                            }
                           >
-                            <div className="flex justify-between text-sm mb-1">
-                              <span className="font-mono text-xs">{tc.id}</span>
-                              <span className="uppercase text-xs text-gray-500">
-                                {tc.type}
-                              </span>
-                            </div>
-                            <button
-                              onClick={() =>
-                                setExpandedCaseId(isExpanded ? null : tc.id)
-                              }
-                              className="text-xs px-2 py-1 rounded border hover:bg-gray-50"
+                            {tab.label}
+                          </button>
+                        ))}
+                      </div>
+                      <ul className="flex flex-col gap-4">
+                        {filteredCases?.map((tc) => {
+                          const isExpanded = expandedCaseId === tc.id;
+
+                          return (
+                            <li
+                              key={tc?.id}
+                              className="border border-gray-200 rounded p-3 shadow-sm mb-8"
                             >
-                              {isExpanded ? "Hide details" : "View details"}
-                            </button>
-                            <h3 className="font-semibold mb-1">{tc.title}</h3>
-                            <div className="mb-1">
-                              <p className="font-semibold text-sm">Steps:</p>
-                              <ol className="list-decimal list-inside text-sm text-gray-700">
-                                {tc.steps.map((step, i) => (
-                                  <li key={i}>{step}</li>
-                                ))}
-                              </ol>
-                            </div>
-                            <p className="text-sm">
-                              <span className="font-semibold">Expected:</span>{" "}
-                              {tc.expected}
-                            </p>
-                          </li>
-                        );
-                      })}
+                              <div className="flex justify-between text-sm mb-1">
+                                <span className="font-mono text-xs">
+                                  {tc.id}
+                                </span>
+                                <span className="uppercase text-xs text-gray-500">
+                                  {tc.type}
+                                </span>
+                              </div>
+                              <button
+                                onClick={() =>
+                                  setExpandedCaseId(isExpanded ? null : tc.id)
+                                }
+                                className="text-xs px-2 py-1 rounded border hover:bg-gray-50"
+                              >
+                                {isExpanded ? "Hide details" : "View details"}
+                              </button>
+                              <h3 className="font-semibold mb-1">{tc.title}</h3>
+                              <div className="mb-1">
+                                <p className="font-semibold text-sm">Steps:</p>
+                                <ol className="list-decimal list-inside text-sm text-gray-700">
+                                  {tc.steps.map((step, i) => (
+                                    <li key={i}>{step}</li>
+                                  ))}
+                                </ol>
+                              </div>
+                              <p className="text-sm">
+                                <span className="font-semibold">Expected:</span>{" "}
+                                {tc.expected}
+                              </p>
+                            </li>
+                          );
+                        })}
+                      </ul>
                     </ul>
-                  </ul>
-                  {filteredCases.length === 0 ? (
-                    <p className="text-sm text-gray-500">
-                      No {filterType === "all" ? "" : filterType} test cases in
-                      this suite.
-                    </p>
-                  ) : (
-                    <ul>...</ul>
-                  )}
-                </>
-              )
-            )}
+                    {filteredCases.length === 0 ? (
+                      <p className="text-sm text-gray-500">
+                        No {filterType === "all" ? "" : filterType} test cases
+                        in this suite.
+                      </p>
+                    ) : (
+                      <ul>...</ul>
+                    )}
+                  </>
+                )
+              )}
+            </section>
           </section>
         </section>
-      </section>
+      </div>
     </main>
   );
 }
