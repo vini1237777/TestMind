8a13f59308384201fa76b1b5c46bcd6e43eba7f9
Vinisha Yadav <vinishadv@gmail.com>
2025-12-06 18:41:07 +0530

    added ecosystem.config.js for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+export const apps = [
+  {
+    name: "testmind",
+    script: "node_modules/next/dist/bin/next",
+    args: "start -p 3000",
+    instances: "max",
+    exec_mode: "cluster",
+    env: {
+      NODE_ENV: "production",
+    },
+  },
+];
