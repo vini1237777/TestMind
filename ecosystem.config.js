f45152be77997490264f6b50fd4c39041f04b46e
Vinisha Yadav <vinishadv@gmail.com>
2025-12-06 20:42:12 +0530

    Changes for Dockerfile.prod for cluster

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,12 @@ export const apps = [
   {
     name: "testmind",
     script: "server.js",
-    instances: "max",
     exec_mode: "cluster",
+    instances: 2,
+    max_memory_restart: "350M",
     env: {
       NODE_ENV: "production",
+      PORT: process.env.PORT || 8080,
     },
   },
 ];
