c64032f5eac8de8189afdf88be23b50afe2b8375
Vinisha Yadav <vinishadv@gmail.com>
2026-03-23 11:06:52 +0530

    chore: add @sentry/nextjs dependency to package.json

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   output: "standalone",
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: "73a69f221555",
+  project: "testmind",
+  silent: !process.env.CI,
+  widenClientFileUpload: true,
+  tunnelRoute: "/monitoring",
+  webpack: {
+    automaticVercelMonitors: true,
+    treeshake: {
+      removeDebugLogging: true,
+    },
+  },
+});
